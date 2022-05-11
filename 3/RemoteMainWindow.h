bed3dcf0e84203590cd63cef74cec2d6423b95a3
Igor Vereshchagin <igor.a.vereshchagin@gmail.com>
2022-05-11 11:21:30 +0300

    cpp_37.4.3 done

--- 3/RemoteMainWindow.h
+++ 3/RemoteMainWindow.h
@@ -9,11 +9,13 @@
 #include <QtWidgets/QLCDNumber>
 #include <QtWidgets/QProgressBar>
 #include <cassert>
+#include <QTimer>
 
 class RemoteMainWindow : public QMainWindow
 {
   Q_OBJECT
 private:
+  QTimer timer;
   QLCDNumber* channelNumberIndicator = nullptr;
   QProgressBar* soundVolumeIndicator = nullptr;
   static const int maxChannelNumber = 999;
@@ -28,11 +30,18 @@ private:
       newChannelNumber = newChannelNumber * 10 + dig;
       channelNumberIndicator->setSegmentStyle(QLCDNumber::Outline);
       channelNumberIndicator->display(newChannelNumber);
+      if (timer.isActive())
+        timer.stop();
+      timer.setInterval(2000);
+      timer.setSingleShot(true);
+      timer.start();
     }
   }
 
 public:
-  RemoteMainWindow(QWidget* parent = nullptr) : QMainWindow(parent) {}
+  RemoteMainWindow(QWidget* parent = nullptr) : QMainWindow(parent), timer(this), channelNumber(0), newChannelNumber(0) {
+    QObject::connect(&timer, &QTimer::timeout, this, &RemoteMainWindow::slot_timeout);
+  }
   void setChannelNumberIndicator(QLCDNumber* lcdNumber) {channelNumberIndicator = lcdNumber;}
   void setSoundVolumeIndicator(QProgressBar* progressBar) {soundVolumeIndicator = progressBar;}
 public slots:
@@ -46,10 +55,43 @@ public slots:
   void slot_add7() {    appendDigit(7);  }
   void slot_add8() {    appendDigit(8);  }
   void slot_add9() {    appendDigit(9);  }
-  void slot_next() {}
-  void slot_prev() {}
-  void slot_vol_up() {}
-  void slot_vol_down() {}
+  void slot_timeout() {
+    assert(channelNumberIndicator != nullptr);
+    channelNumber = newChannelNumber;
+    newChannelNumber = 0;
+    channelNumberIndicator->setSegmentStyle(QLCDNumber::Flat);
+    channelNumberIndicator->display(channelNumber);
+  }
+  void slot_next() {
+    assert(channelNumberIndicator != nullptr);
+    if (channelNumber < maxChannelNumber)
+    {
+      newChannelNumber = 0;
+      channelNumber++;
+      channelNumberIndicator->setSegmentStyle(QLCDNumber::Flat);
+      channelNumberIndicator->display(channelNumber);
+      if (timer.isActive())
+        timer.stop();
+    }
+  }
+  void slot_prev() {
+    assert(channelNumberIndicator != nullptr);
+    if (channelNumber > 0)
+    {
+      newChannelNumber = 0;
+      channelNumber--;
+      channelNumberIndicator->setSegmentStyle(QLCDNumber::Flat);
+      channelNumberIndicator->display(channelNumber);
+      if (timer.isActive())
+        timer.stop();
+    }
+  }
+  void slot_vol_up() {
+    soundVolumeIndicator->setValue(soundVolumeIndicator->value() + 10);
+  }
+  void slot_vol_down() {
+    soundVolumeIndicator->setValue(soundVolumeIndicator->value() - 10);
+  }
 
 };
 
